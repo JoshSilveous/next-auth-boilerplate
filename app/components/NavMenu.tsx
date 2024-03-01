7ebe7683e80887cd9c2adfd70f63d55d749537b3
Joshua Silveous <JoshSilveous@gmail.com>
2024-03-01 17:07:55 -0500

    added more pages and styles

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -2,30 +2,55 @@
 import Link from 'next/link'
 import styles from './NavMenu.module.scss'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { usePathname } from 'next/navigation'
 
-function SignInButton() {
-	return <button onClick={() => signIn()}>Sign in</button>
-}
-
-function SignOutButton() {
-	return <button onClick={() => signOut()}>Sign out</button>
-}
+const pages = [
+	{ name: 'Home', href: '/' },
+	{ name: 'Transactions', href: '/transactions' },
+	{ name: 'Categories', href: '/categories' },
+	{ name: 'Accounts', href: '/accounts' },
+]
 
 export default function NavMenu() {
 	const { data: session } = useSession()
+	const pathname = usePathname()
 	const signedIn = session !== null
 	return (
-		<div>
+		<div className={styles.menu}>
 			{!signedIn ? (
-				<SignOutButton />
+				<SignInButton />
 			) : (
 				<>
-					<Link href='/' passHref>
-						<button>Home</button>
-					</Link>
-					<SignInButton />
+					<div className={styles.link_container}>
+						{pages.map((page, index) => {
+							return (
+								<Link key={index} href={page.href} passHref>
+									<div className={pathname === page.href ? styles.current : ''}>
+										<div className={styles.text}>{page.name}</div>
+									</div>
+								</Link>
+							)
+						})}
+					</div>
+					<SignOutButton />
 				</>
 			)}
 		</div>
+	)
+}
+
+function SignInButton() {
+	return (
+		<button className={styles.sign_in} onClick={() => signIn()}>
+			Sign in
+		</button>
+	)
+}
+
+function SignOutButton() {
+	return (
+		<button className={styles.sign_out} onClick={() => signOut()}>
+			Sign out
+		</button>
 	)
 }
