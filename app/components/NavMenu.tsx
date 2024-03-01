21c37ab78e98a806d4aaead6a9d33e966410b554
Joshua Silveous <JoshSilveous@gmail.com>
2024-03-01 15:54:21 -0500

    removed mui

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -1,30 +1,31 @@
 'use client'
 import Link from 'next/link'
+import styles from './NavMenu.module.scss'
 import { signIn, signOut, useSession } from 'next-auth/react'
 
-function AuthButton() {
-	const { data: session } = useSession()
+function SignInButton() {
+	return <button onClick={() => signIn()}>Sign in</button>
+}
 
-	if (session) {
-		return (
-			<>
-				{session?.user?.name} <br />
-				<button onClick={() => signOut()}>Sign out</button>
-			</>
-		)
-	}
-	return (
-		<>
-			Not signed in <br />
-			<button onClick={() => signIn()}>Sign in</button>
-		</>
-	)
+function SignOutButton() {
+	return <button onClick={() => signOut()}>Sign out</button>
 }
 
 export default function NavMenu() {
+	const { data: session } = useSession()
+	const signedIn = session !== null
 	return (
 		<div>
-			<AuthButton />
+			{!signedIn ? (
+				<SignOutButton />
+			) : (
+				<>
+					<Link href='/' passHref>
+						<button>Home</button>
+					</Link>
+					<SignInButton />
+				</>
+			)}
 		</div>
 	)
 }
